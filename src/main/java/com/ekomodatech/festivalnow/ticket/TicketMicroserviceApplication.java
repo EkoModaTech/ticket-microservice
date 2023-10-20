fbf283d93de65d4fea1d414f917e25b75376dd0a
sdsuarez <suarezds@javeriana.edu.co>
2023-10-20 11:39:34 -0500

    ticket base

---
+++ src/main/java/com/ekomodatech/festivalnow/ticket/TicketMicroserviceApplication.java
@@ -0,0 +1,12 @@
+package com.ekomodatech.festivalnow.ticket;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class TicketMicroserviceApplication {
+    public static void main(String[] args) {
+		SpringApplication.run(TicketMicroserviceApplication.class, args);
+	}
+}
